f473b32bdaba0c72d39c91c0e733a956fece5625
sarapuertas <saritapuertas@hotmail.com>
2022-09-08 14:07:33 +0200

    service layer

---
+++ internal/comment/comment.go
@@ -0,0 +1,41 @@
+package comment
+
+import (
+	"context"
+	"fmt"
+)
+
+// Comment - structure of the service
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+// Service - struct on which all the logic will be built
+type Service struct {
+	Store Store
+}
+
+// NewService - returns a pointer to a new service
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("retrieving a comment")
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, err
+	}
+
+	return cmt, nil
+}
